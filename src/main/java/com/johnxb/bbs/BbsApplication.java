2f91fc9861f3aef9bdcddbfea96925505a5c902f
john <408106378@qq.com>
2018-12-20 09:53:06 +0800

    swagger配置和跨域

--- src/main/java/com/johnxb/bbs/BbsApplication.java
+++ src/main/java/com/johnxb/bbs/BbsApplication.java
@@ -2,13 +2,54 @@ package com.johnxb.bbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@Configuration
+@EnableSwagger2
 public class BbsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BbsApplication.class, args);
 	}
-
+	@Bean
+	public Docket testApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.genericModelSubstitutes(DeferredResult.class)
+				.useDefaultResponseMessages(false)
+				.forCodeGeneration(true)
+				.pathMapping("/")// base，最终调用接口后会和paths拼接在一起
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.johnxb.bbs.api"))
+				.build();
+	}
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("BBS Api")
+				.description("BBS Api文档")
+				.termsOfServiceUrl("")
+				.version("1.0")
+				.build();
+	}
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
 }
 
