81fd804f75a4b5d457f78e2e0ee36300c9b9e867
john <408106378@qq.com>
2018-12-29 17:00:14 +0800

    修改命名

--- bbs-api/src/main/java/com/johnxb/bbs/api/security/MyAccessDeniedHandler.java
+++ bbs-api/src/main/java/com/johnxb/bbs/api/security/MyAccessDeniedHandler.java
@@ -13,13 +13,11 @@ import java.io.PrintWriter;
 
 @Component
 public class MyAccessDeniedHandler implements AccessDeniedHandler{
-
     @Override
     public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
         httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        PrintWriter writer = httpServletResponse.getWriter();
         System.out.println("自定义异常");
         httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN,
-                e.getMessage());;
+                e.getMessage());
     }
 }
