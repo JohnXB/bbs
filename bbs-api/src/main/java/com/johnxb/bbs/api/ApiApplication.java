9fa1d350c9c4ce617a429c2706539c1e1a159493
john <408106378@qq.com>
2019-01-07 14:28:06 +0800

    添加请求头到swagger中

--- bbs-api/src/main/java/com/johnxb/bbs/api/ApiApplication.java
+++ bbs-api/src/main/java/com/johnxb/bbs/api/ApiApplication.java
@@ -11,12 +11,18 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication(scanBasePackages = {"com.johnxb.bbs"})
 @Configuration
@@ -30,10 +36,18 @@ public class ApiApplication {
 
     @Bean
     public Docket testApi() {
+        ParameterBuilder par = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        par.name("Authorization").description("用户令牌")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .defaultValue("Bearer")
+                .required(false).build(); //header中的ticket参数非必填，传空也可以
+        pars.add(par.build());    //根据每个方法名也知道当前方法在设置什么参数
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .genericModelSubstitutes(DeferredResult.class)
                 .useDefaultResponseMessages(false)
+                .globalOperationParameters(pars)
                 .forCodeGeneration(true)
                 .pathMapping("/")// base，最终调用接口后会和paths拼接在一起
                 .select()
