0270a9b04ffba14578f61cfcb25731348acdcec6
john <408106378@qq.com>
2019-01-29 10:50:42 +0800

    日志

--- bbs-dao/src/main/java/com/johnxb/bbs/dao/BbsDaoConfiguration.java
+++ bbs-dao/src/main/java/com/johnxb/bbs/dao/BbsDaoConfiguration.java
@@ -1,7 +1,6 @@
 package com.johnxb.bbs.dao;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
