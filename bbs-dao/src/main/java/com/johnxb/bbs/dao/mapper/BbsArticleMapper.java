e0e1ed90272292f50b85a5d4384bacfc59d57175
john <408106378@qq.com>
2019-02-12 15:53:11 +0800

    增加启动彩蛋

--- bbs-dao/src/main/java/com/johnxb/bbs/dao/mapper/BbsArticleMapper.java
+++ bbs-dao/src/main/java/com/johnxb/bbs/dao/mapper/BbsArticleMapper.java
@@ -3,10 +3,12 @@ package com.johnxb.bbs.dao.mapper;
 import com.johnxb.bbs.dto.Common.GetArticleDto;
 import com.johnxb.bbs.entity.BbsArticle;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 
 public interface BbsArticleMapper {
+    // 通过tag查询
     List<BbsArticle> getArticleByTag(@Param("tagId") Integer tagId,@Param("articleQuery") GetArticleDto articleDto);
 
     /**
@@ -15,5 +17,15 @@ public interface BbsArticleMapper {
      */
     BbsArticle getArticleInfoById(@Param("articleId")Integer articleId);
 
+    /**
+     * 创建文章
+     */
     Integer createArticle(BbsArticle bbsArticle);
+
+    /**
+     * @param start 偏移量，分页
+     * @param count
+     * @return
+     */
+    List<BbsArticle> getArticleByUser(@Param("userId")Integer userId, @Param("start")Integer start,@Param("count")Integer count);
 }
